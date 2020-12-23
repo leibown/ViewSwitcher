d6a2f5a180910bfc1c332881ce9f8f0db902eaf4
Leibown <leibown.leila@gmail.com>
2020-12-23 15:39:44 +0800

    first commit

--- app/src/main/java/com/leibown/viewswitcher/MainActivity.java
+++ app/src/main/java/com/leibown/viewswitcher/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-//        RecyclerView
+//        RecyclerView.LayoutManager
 
 
         switcherHelper.addView(ChildViewFactory.create(this, R.layout.layout_123, null));
